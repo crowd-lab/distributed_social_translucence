a8c564d2efdd80ad5619d7dea674cad608f5b1a0
christopherhurt <chris526@vt.edu>
2018-11-09 16:56:33 -0500

    Fixed runtime issues

--- accountability.py
+++ accountability.py
@@ -211,10 +211,11 @@ def wait():
     if TURK_ID_VAR not in session.keys():
         session[TURK_ID_VAR] = request.args.get(TURK_ID_VAR)
     uid = session[TURK_ID_VAR]
+    cond = request.args.get(CONDITION_VAR)
 
     if was_observer is not None:
         cond = CONDITION_EXP_VAL
-    else:
+    elif cond is None:
         cond = CONDITION_CON_VAL if random.random() < 0.5 else CONDITION_EXP_VAL
 
     # Assigning job based on whether an unpaired individual exists
@@ -296,6 +297,10 @@ def accept_moderations():
 
     pair_id = json['pair_id']
     img_ids = json['img_ids']
+
+    # TODO: remove
+    print('Here\'s the image IDs: ' + str(img_ids))
+
     decisions = json['decisions']
     for i in range(NUM_IMAGES):
         if pair_id == 0:
@@ -453,7 +458,8 @@ def work():
     # img_subset = [s[0].encode("ascii") for s in subset]
     img_subset = [str(s[0]) for s in subset]
     print('Images in db: %s' % img_subset)
-    img_ids = query_db('select img_id from images where img_path in (%s)' % ('"' + '", "'.join(img_subset) + '"'), one=True)[0]
+    #img_ids = query_db('select img_id from images where img_path in (%s)' % ('"' + '", "'.join(img_subset) + '"'), one=True)[0]
+    img_ids = [query_db('select img_id from images where img_path=?', [img_subset[i]], one=True)[0] for i in range(len(img_subset))]
     print('Image IDs: %s' % img_ids)
 
     # check if first or last (first is pair_id=1, last is marked by URL param isLast), mark and update edge_case accordingly (0 or 1)
