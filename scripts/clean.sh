0a72dbfe62da762b785cb05eb0bb258e13d584a8
christopherhurt <chris526@vt.edu>
2019-03-04 12:22:29 -0500

    Fix clean.sh, revert TogetherJS testing

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,2 @@
-# Delete any existing database and launch the application
-heroku pg:reset --confirm jit-accountability
-heroku restart
+# Delete any existing database and relaunch the application
+heroku pg:reset --confirm jit-accountability && heroku pg:psql -c "vacuum;" && heroku ps:restart web
