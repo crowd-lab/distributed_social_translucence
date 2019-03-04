b2da146dca4a3b093419ebd10fa5012150b52df0
Jacob Thebault-Spieker <jtheabultspieker@vt.edu>
2019-03-04 13:53:46 -0500

    move DB initialization to before first request

--- accountability.py
+++ accountability.py
@@ -49,6 +49,30 @@ JOB_OBS_VAL = 'obs'
 CONDITION_CON_VAL = 'con'
 CONDITION_EXP_VAL = 'exp'
 
+@app.before_first_request
+def build_db():
+    with app.app_context():
+    db = get_db()
+
+    # Load database schema
+    # db.execute(sqlalchemy.text('create table if not exists images (img_id serial primary key, path text unique, text text, poster text, affiliation text);'))
+    # db.execute(sqlalchemy.text('create table if not exists participants (user_id serial primary key, turk_id text unique, condition text, edge_case text, disconnected boolean);'))
+    # db.execute(sqlalchemy.text('create table if not exists pairs (id serial primary key, obs_id integer unique references participants(user_id), mod_id integer unique references participants(user_id), obs_submitted boolean, mod_submitted boolean, work_ready boolean, mod_ready boolean, obs_ready boolean, last_mod_time real, last_obs_time real, disconnect_occurred boolean, create_time numeric);'))
+    # db.execute(sqlalchemy.text('create table if not exists observations(id serial primary key, pair_id integer references pairs(id), obs_text text, img_id integer, agreement_text text);'))
+    # db.execute(sqlalchemy.text('create table if not exists moderations(id serial primary key, decision text, img_id integer references images(img_id), pair_id integer references pairs(id));'))
+    # db.execute(sqlalchemy.text('create table if not exists chosen_imgs(id serial primary key, img_id integer, pair_id integer);'))
+    # db.execute(sqlalchemy.text('create table if not exists images_revealed(id serial primary key, pair_id integer, img_index integer, temp_decision text);'))
+    # db.execute(sqlalchemy.text('create table if not exists consent(id serial primary key, turk_id text unique, response text);'))
+
+    with open('db.schema', mode='r') as f:
+        results = db.execute(sqlalchemy.text(f.read()))
+
+    # Load images (if none are loaded)
+    out = db.execute('select count(*) from images')
+    count = out.fetchall()[0][0]
+    if count == 0:
+        load_images_to_db()
+
 # Close database
 @app.teardown_appcontext
 def close_connection(exception):
@@ -80,30 +104,7 @@ def load_images_to_db():
     conn.commit()
 
 # App initialization
-with app.app_context():
-    db = get_db()
 
-    # Load database schema
-    db.execute(sqlalchemy.text('create table if not exists images (img_id serial primary key, path text unique, text text, poster text, affiliation text);'))
-    db.execute(sqlalchemy.text('create table if not exists participants (user_id serial primary key, turk_id text unique, condition text, edge_case text, disconnected boolean);'))
-    db.execute(sqlalchemy.text('create table if not exists pairs (id serial primary key, obs_id integer unique references participants(user_id), mod_id integer unique references participants(user_id), obs_submitted boolean, mod_submitted boolean, work_ready boolean, mod_ready boolean, obs_ready boolean, last_mod_time real, last_obs_time real, disconnect_occurred boolean, create_time numeric);'))
-    db.execute(sqlalchemy.text('create table if not exists observations(id serial primary key, pair_id integer references pairs(id), obs_text text, img_id integer, agreement_text text);'))
-    db.execute(sqlalchemy.text('create table if not exists moderations(id serial primary key, decision text, img_id integer references images(img_id), pair_id integer references pairs(id));'))
-    db.execute(sqlalchemy.text('create table if not exists chosen_imgs(id serial primary key, img_id integer, pair_id integer);'))
-    db.execute(sqlalchemy.text('create table if not exists images_revealed(id serial primary key, pair_id integer, img_index integer, temp_decision text);'))
-    db.execute(sqlalchemy.text('create table if not exists consent(id serial primary key, turk_id text unique, response text);'))
-
-    # with open('db.schema', mode='r') as f:
-    #     results = db.execute(sqlalchemy.text(f.read()))
-        
-        # db.cursor().executescript(f.read())
-    # db.commit()
-
-    # Load images (if none are loaded)
-    out = db.execute('select count(*) from images')
-    count = out.fetchall()[0][0]
-    if count == 0:
-        load_images_to_db()
 
 # Gets subset of all images to be displayed
 def get_array_subset(array, num_vals, cannot_contain):
