10104eb402c7923d4b44eb77725e6ed4329ade53
Paul Blackburn <paul89@vt.edu>
2020-02-22 21:40:41 +0000

    Change environment variables

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,4 +1,4 @@
 # Designate Flask application and launch on localhost
 export FLASK_APP=accountability.py
-export DATABASE_URL=$(heroku config:get DATABASE_URL)
-python3 -m flask run
+export DATABASE_URL="postgres:///pairwise" #postgres://paul@localhost:5432/pairwise
+python3 -m flask run --host=0.0.0.0
